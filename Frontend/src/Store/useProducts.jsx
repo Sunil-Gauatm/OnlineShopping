62ba6a3e9077d9bfa7528f0812d22db7e3d52762
Sunil Gautam <gautamsunil917@gmail.com>
2025-05-10 22:13:05 +0545

    dynamicNewArrivals

---
+++ Frontend/src/Store/useProducts.jsx
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+import Product from "../assets/Product.png";
+
+export const useProduct = create((set) => {
+  // Initial Product Data
+  const initialProducts = {
+    "Men’s Fashion": [
+      {
+        img: Product,
+        name: "White Dress",
+        brandname: "Al Karam",
+        Review: "4.1",
+        price: "95.50",
+      },
+      {
+        img: Product,
+        name: "Black T-Shirt",
+        brandname: "Zara",
+        Review: "4.5",
+        price: "35.20",
+      },
+    ],
+    "Women’s Fashion": [
+      {
+        img: Product,
+        name: "Red Gown",
+        brandname: "Versace",
+        Review: "4.8",
+        price: "150.00",
+      },
+    ],
+    "Kids’ Fashion": [
+      {
+        img: Product,
+        name: "Blue Hoodie",
+        brandname: "H&M",
+        Review: "4.2",
+        price: "45.00",
+      },
+    ],
+    Accessories: [
+      {
+        img: Product,
+        name: "Leather Belt",
+        brandname: "Gucci",
+        Review: "4.6",
+        price: "55.00",
+      },
+    ],
+    Footwear: [
+      {
+        img: Product,
+        name: "Sneakers",
+        brandname: "Nike",
+        Review: "4.7",
+        price: "120.00",
+      },
+    ],
+  };
+
+  // Get the first category dynamically
+  const firstCategory = Object.keys(initialProducts)[0];
+
+  return {
+    allProducts: initialProducts,
+    selectedCategories: [firstCategory], // Default to the first category
+    setSelectedCategories: (categories) => set({ selectedCategories: categories }),
+  };
+});
--- Frontend/src/components/NewArrivals.jsx
+++ Frontend/src/components/NewArrivals.jsx
@@ -1,77 +1,81 @@
-import React from 'react'
-import Produt from '../assets/Product.png'
+import React, { useState } from "react";
+import Produt from "../assets/Product.png";
+import { useProduct } from "../Store/useProducts";
 
 const NewArrivals = () => {
+  const { allProducts, selectedCategories, setSelectedCategories } =
+    useProduct();
+
+  // console.log(selectedCategories)
+  console.log(allProducts);
+
   return (
-    <div className='h-[50rem] mx-50 mt-14'>
-        <div className='flex flex-col justify-center items-center'>
-
-        <div className='text-[2.5rem] font-logo'>
-            New Arrivals
+    <div className="h-[50rem] mx-50 mt-14">
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-[2.5rem] font-logo">New Arrivals</div>
+        <div className="text-[1.2rem] text-gray-700 mx-50 mt-2 text-center ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque
+          duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices
+          sollicitudin
         </div>
-        <div className='text-[1.2rem] text-gray-700 mx-50 mt-2 text-center '>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin 
-        </div>
-        </div>
-        {/* buttons */}
-        <div className='mt-10 flex flex-row gap-6 justify-center flex-wrap'>
+      </div>
+      {/* buttons */}
 
-            <button className='bg-gray-200 px-12 py-4 rounded-[0.4rem] flex justify-center items-center'>
-                Men’s Fashion
+      <div className="mt-10 flex flex-row gap-6 justify-center flex-wrap">
+        {Object.keys(allProducts).map((data, index) => {
+          return (
+            <button
+              className={`${
+                selectedCategories == data
+                  ? "bg-black  text-gray-200   px-12 py-4   shadow shadow-black/90 rounded-[0.4rem] flex justify-center items-center"
+                  : "bg-gray-200 px-12 py-4 rounded-[0.4rem] flex justify-center items-center"
+              }`}
+              key={index}
+              onClick={() => setSelectedCategories([data])}
+            >
+              {data}
             </button>
-            <button className='bg-black  text-gray-200   px-12 py-4   shadow shadow-black/90 rounded-[0.4rem] flex justify-center items-center'>
-                Men’s Fashion
-            </button>
-            <button className='bg-gray-200 px-12 py-4 rounded-[0.4rem] flex justify-center items-center'>
-                Men’s Fashion
-            </button>
-            <button className='bg-gray-200 px-12 py-4 rounded-[0.4rem] flex justify-center items-center'>
-                Men’s Fashion
-            </button>
-            <button className='bg-gray-200 px-12 py-4 rounded-[0.4rem] flex justify-center items-center'>
-                Men’s Fashion
-            </button>
+          );
+        })}
+      </div>
 
-        </div>
-        <div className='flex flex-row gap-10 mt-8 flex-wrap justify-center items-center '>
+      <div className="flex flex-row gap-10  flex-wrap justify-center items-center py-15 ">
+        {allProducts[selectedCategories]?.map((data, index) => {
+          return (
+            <div
+              className="box bg-white shadow-xl w-[21rem] flex flex-col box-border px-6 rounded-lg pb-6"
+              key={index}
+            >
+              <img
+                src={data.img}
+                alt="Product"
+                className="w-[21rem] self-center rounded-xl"
+              />
 
-            <div className='box bg-white shadow-xl w-[21rem] flex flex-col box-border px-6 rounded-lg pb-6'>
-               <img src={Produt}alt="Product" className='w-[21rem] self-center rounded-xl' />
+              <div className="flex flex-row justify-between mt-2">
+                <div className="flex flex-col">
+                  <span>{data.name}</span>
+                  <span className="text-[0.8rem] text-gray-500">
+                    {data.brandname}
+                  </span>
+                </div>
 
-              <div className='flex flex-row justify-between mt-2'>
-
-               <div className='flex flex-col'>
-                <span>
-                    White Dress
-                </span>
-                <span className='text-[0.8rem] text-gray-500'>
-                    Al Karam
-                </span>
-               </div>
-
-               <div>
-                rating
-               </div>
-
+                {/* <div>rating</div> */}
               </div>
 
-              <div className='mt-2'>
-                (4.1k) Customer Reviews
+              <div className="mt-2 text-[0.9rem]">
+                ({data.Review}k) Customer Reviews
               </div>
-
-              <div className='flex flex-row justify-between mt-2'>
-                <span>
-                    $95.50
-                </span>
-                <span>Almost Sold Out</span>
+              <div className="flex flex-row justify-between mt-2 items-end  ">
+                <span className="font-bold text-[1.6rem]">${data.price}</span>
+                <span className="text-red-500">Almost Sold Out</span>
               </div>
             </div>
-
-     
-        </div>
-
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewArrivals
+export default NewArrivals;
